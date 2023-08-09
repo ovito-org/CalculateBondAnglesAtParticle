c802ebf9fdbd8a8bd257c3a5bd499e80e2f4e984
Constanze Kalcher <c.kalcher@googlemail.com>
2023-08-09 10:12:10 +0200

    Initital Commit

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
-# Python Modifier Template
+# Calculate Bond Angles At Particle
+This *OVITO* [Python modifier](https://docs.ovito.org/python/introduction/custom_modifiers.html) outputs the angles between all pairwise combinations of bonds at one particle.
 
-Template for a custom Python-based modifier that hooks into OVITO and can easily be shared with other users.
+## Description
+Python modifier for *OVITO* that looks up all bonds connected to the specified particle and calculates the angles between all pairwise combinations of those bonds. In other words, it returns the bond pair ids, particle triplet ids and angles for all particle triplets A--B--C that have the specified particle as center particle (B).
 
-This repository contains a template for creating your own [Python script modifier](https://docs.ovito.org/python/introduction/custom_modifiers.html), 
-which can be installed into *OVITO Pro* or the [`ovito`](https://pypi.org/project/ovito/) Python module using *pip*.
+## Parameters 
+- `center_particle` / "Compute for particle": The particle index or identifier of the central particle that all bonds are attached to.
+- `mode` / "Choose particle by: Index/Identifier": Choose wheter input center particle is specified by index or identifier.
+- `bond_mode` / "List bonds by: Index/Identifier": Choose wheter output bond pairs are listed by their index or identifier.
 
-## Getting Started
+## Example
+![Screenshot of OVITO Pro Desktop application](./Examples/CalculateBondAnglesAtParticlesModifier.png)
 
-1. Click the "Use this template" button to create your own repository based on this template.
-2. Rename `src/ModifierName` to reflect the name of your modifier.
-3. Implement your [modifier](https://docs.ovito.org/python/introduction/custom_modifiers.html#advanced-interface) in [`src/ModifierName/__init__.py`](src/ModifierName/__init__.py). If your modifier needs access to more than one frame of a trajectory, you can uncomment and implement the `input_caching_hints` method. Otherwise, you can delete it. More details on this method can be found in the [OVITO Python docs](https://www.ovito.org/docs/current/python/introduction/custom_modifiers.html#writing-custom-modifiers-advanced-interface). 
-4. Fill in the [`pyproject.toml`](pyproject.toml) file. Fields that need to be replaced with your information are enclosed in descriptive `[[field]]` tags. Please make sure to include ovito>=3.9 as a dependency. Depending on your needs, you can add additional fields to the `pyproject.toml` file. Information can be found [here](https://setuptools.pypa.io/en/latest/userguide/index.html).
-5. Fill in the [`README_Template.md`](README_Template.md) file. Again, the `[[fields]]` placeholders should guide you. Feel free to add other sections like "Images", "Citation", or "References" as needed.
-6. Add meaningful examples and data sample files to the `Examples` directory to help others understand the use of your modifier.
-7. Pick a license for your project and replace the current (MIT) [`LICENSE`](LICENSE) file with your license. If you keep the MIT license, please update the name and year in the current file.
-8. Once you're done, rename `README_Template.md` to `README.md`, replacing this file.
+## Installation
+- OVITO PRO built-in Python interpreter
+```
+ovitos -m pip install --user git+https://github.com/ckalcher/CalculateBondAnglesAtParticle.git
+``` 
+- Standalone Python package or Conda environment
+```
+pip install --user git+https://github.com/ckalcher/CalculateBondAnglesAtParticle.git
+```
+- Please note that the `--user` tag is recommended but optional and depends on your Python installation.
+
+## Technical information / dependencies
+- Tested on OVITO version 3.9.1
+
+## Contact
+Constanze Kalcher kalcher@ovito.org
---
+++ src/CalculateBondAnglesAtParticle/__init__.py
@@ -0,0 +1,104 @@
+### Calculate Bond Angles At Particle
+# This modifier outputs the angles between all pairwise combinations of bonds at one particle.
+
+from ovito.data import *
+import numpy as np 
+from traits.api import Union, Int, Enum, Bool
+from ovito.pipeline import ModifierInterface
+from itertools import combinations
+
+class CalculateBondAnglesAtParticle(ModifierInterface):
+   
+    center_particle = Union(None, Int, label = "Compute for particle")
+    mode = Enum("Index", "Identifier", label = "Choose particle by")
+    bond_mode = Enum("Index", "Identifier", label = "List bonds by")
+
+    def calculate_bond_angles(self, bond_vectors):
+        b1 = bond_vectors[:,0]/np.linalg.norm(bond_vectors[:,0], axis = 1)[:, None]
+        b2 = bond_vectors[:,1]/np.linalg.norm(bond_vectors[:,1], axis = 1)[:, None]
+        return np.degrees(np.arccos(np.sum(b1*b2, axis = 1)))
+    
+    def calculate_bond_vector_combinations(self, data, particle):
+        # Create bonds enumerator object.
+        bonds_enum = BondsEnumerator(data.particles.bonds)
+        # List of bond indices of all bonds at the current particle
+        bonds_of_particle = [bond_index for bond_index in bonds_enum.bonds_of_particle(particle)]
+        if len(bonds_of_particle) < 2:
+            raise RuntimeError("Not enough bonds found to compute angles.")
+        # All possible bond pairs in bonds_of_particle list
+        bond_pairs = list(combinations(bonds_of_particle, 2))
+        # Look up corresponding bond vectors
+        topo = data.particles.bonds.topology[bond_pairs] 
+        # Flip bond vector if current particle is not index 0 in topology pairs
+        idx = np.where(topo[:,:,0] != particle)
+        vectors = data.particles.bonds["Bond vectors"][bond_pairs]
+        vectors[idx[0], idx[1]] *= -1
+        # Get particle index triplets from topo [B,A][B,C]
+        topo[idx[0], idx[1]] =  np.flip(topo[idx[0], idx[1]])
+        triplets = np.column_stack((topo[:, 0, 1],topo[:, 1, :]))
+        return bond_pairs, triplets, vectors
+       
+    def modify(self, data, **kwargs):
+
+        if self.center_particle == None:
+            return
+        if self.mode == "Identifier" and "Particle Identifier" not in data.particles:
+            raise RuntimeError("No Particle Identifiers in DataCollection. Deactivate Option <Output Particle Identifiers>.")
+        if self.mode == "Identifier" and (np.isin(data.particles.identifiers, self.center_particle).any() == False):
+            raise IndexError("Invalid Particle Identifier")
+        if self.mode == "Index" and (self.center_particle >= data.particles.count or self.center_particle < 0):
+            raise IndexError(f"Invalid Particle Index. Choose Index between 0 and {data.particles.count-1}.")
+        if data.particles.bonds == None:
+            raise RuntimeError("No Bonds in DataCollection. Please first generate bonds.")
+        if self.bond_mode == "Identifier" and "Bond Identifier" not in data.particles.bonds:
+            raise RuntimeError("No Bond Identifiers in DataCollection. Switch to Option <Output Bond Indices>.")
+
+        # Look up bond indices of all bonds connected to current particle
+        if self.mode == 'Identifier':
+            particle = np.where(data.particles.identifiers == self.center_particle)[0][0]
+        else:
+            particle = self.center_particle
+
+        # Calculate bond vectors for global topology array    
+        positions = data.particles.positions
+        topology = data.particles.bonds.topology
+        bond_vectors = positions[topology[:,1]] - positions[topology[:,0]]
+        if data.particles.bonds.pbc_vectors != None:
+            bond_vectors += np.dot(data.cell[:3,:3], data.particles.bonds.pbc_vectors.T).T
+        data.particles_.bonds_.create_property("Bond vectors", data = bond_vectors)    
+        # Get all possible combinations of bond pairs at one particle, the corresponding 
+        # particle index triplets and bond vectors
+        bond_pairs, triplets, v_b = self.calculate_bond_vector_combinations(data, particle)
+        # Calculcate angles between all pairs of bond vectors
+        angles = self.calculate_bond_angles(v_b)
+        
+        #Script output
+        b = len(str(data.particles.bonds.count))
+        p = len(str(data.particles.count))
+        header = f"Triplet A-B-C{(2*p+10+len(str(particle))-13)*' '}Angle{7*' '}Bond Pair B1 - B2" 
+        print(header)
+        print(f"{'-'*len(header)}")
+        for i in range(len(bond_pairs)):
+            A,B,C = triplets[i]
+            if self.mode == 'Identifier':
+                A,B,C = data.particles.identifiers[[A, B, C]]
+            output = f"{A:{p}d} - {B} - {C:<{p}d}    {angles[i]:<8.4f}" 
+            B1, B2 = bond_pairs[i][0], bond_pairs[i][1]
+            if self.bond_mode == 'Identifier':
+                B1, B2 = int(data.particles.bonds['Bond Identifier'][B1]),int(data.particles.bonds['Bond Identifier'][B2])
+            output+=f"    {B1:{b}d} - {B2:<{b}d}"
+            print(output)
+        
+        # Store results as OVITO Data Table
+        table = data.tables.create(
+            identifier=f"bond-angles-{self.center_particle}",
+            title=f"Bond Angles of Particle {self.center_particle}",
+            plot_mode=DataTable.PlotMode.NoPlot)
+        table.y = table.create_property('Angle', data=angles)
+        if self.mode == 'Identifier':
+            triplets = data.particles.identifiers[triplets]
+        table.create_property('Particle Triplet', data=triplets, components=['A', 'B', 'C'])
+    
+        if self.bond_mode == 'Identifier':
+            bond_pairs = data.particles.bonds['Bond Identifier'][bond_pairs]
+        table.create_property('Bond Pair', data=bond_pairs, components=['Bond1', 'Bond2'])
